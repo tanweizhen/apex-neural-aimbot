99981a05f6a08b09479941e55896d610f29e3fc2
tanweizhen <wtan009@gmail.com>
2024-02-27 16:54:38 +0800

    use model.track to predict, persist=True for better stability

--- predict.py
+++ predict.py
@@ -7,6 +7,6 @@ def predict_init(args):
     model = YOLO(args.model_dir + args.model, task='detect')
 def predict(args,img):
     global model
-    res = model(img, verbose=args.verbos, half=args.half, iou=args.iou, conf=args.conf, show=args.show)
+    res = model.track(img, verbose=args.verbos, half=args.half, iou=args.iou, conf=args.conf, show=args.show, persist=True, tracker="bytetrack.yaml")
     #print(res)
     return res[0]
