877da4ee425eaf40f7f3a87c7647044844d79b29
tanweizhen <wtan009@gmail.com>
2024-02-27 11:57:35 +0800

    args

--- predict.py
+++ predict.py
@@ -3,6 +3,7 @@ from ultralytics import YOLO
 def predict_init(args):
     global model
     print("Predict on", args.model_dir + args.model)
+    print(args)
     model = YOLO(args.model_dir + args.model, task='detect')
 def predict(args,img):
     global model
